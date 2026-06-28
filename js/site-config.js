0f8f60b51b15a74f5ad098a1ecb34c75fb78186d
omphoolbhandar <ctetone31@gmail.com>
2026-06-29 02:22:54 +0530

    Update site-config.js

--- js/site-config.js
+++ js/site-config.js
@@ -6,7 +6,7 @@
    number 98765 43210 → "919876543210".
    ============================================================ */
 
-const SHOP_WHATSAPP_NUMBER = "91XXXXXXXXXX";
+const SHOP_WHATSAPP_NUMBER = "916202736689";
 const SHOP_NAME = "Om Flower Store";
 
 const OCCASIONS = [
@@ -14,7 +14,7 @@ const OCCASIONS = [
   'Festival', 'Get Well Soon', 'Congratulations', 'Just Because'
 ];
 
-window.SHOP_CONFIGURED = SHOP_WHATSAPP_NUMBER !== "91XXXXXXXXXX";
+window.SHOP_CONFIGURED = SHOP_WHATSAPP_NUMBER !== "916202736689";
 
 function buildWhatsAppLink(message) {
   return `https://wa.me/${SHOP_WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
